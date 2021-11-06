5a972a49e26e744a8f9a096e5a1907703db02fe5
owlingjia <lingjia.ow.2019@sis.smu.edu.sg>
2021-11-06 18:21:58 +0800

    cleaned up imports into a main python file, changed import statements for other notebooks
    to import all modules and packages: from utils import *

---
+++ Analysis/utils.py
@@ -0,0 +1,55 @@
+import os
+import pandas as pd
+import numpy as np
+import string
+import pickle
+
+from collections import Counter
+
+# Packages for Machine Learning
+import sklearn
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import TruncatedSVD
+from sklearn_pandas import DataFrameMapper
+
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+from sklearn.metrics import classification_report,confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+from sklearn.metrics import precision_recall_fscore_support
+
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import FeatureUnion
+
+# Packages for NLP
+import regex as re
+import nltk
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+# nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+# Packages for word2vec
+import gensim
+from gensim.models import word2vec
+import gensim.downloader as api
+
+# Packages for sentiment analysis
+from textblob import TextBlob
+
+# Packages for visualisation 
+from wordcloud import WordCloud, STOPWORDS
+import seaborn as sns
+import matplotlib.pyplot as plt
+# % matplotlib inline
